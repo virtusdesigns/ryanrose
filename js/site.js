8e49e3085945725ac2637bba6691c88c890db75f
Brett Bodine <brett@virtusdesigns.com>
2017-07-29 14:26:55 -0700

    updates

--- js/site.js
+++ js/site.js
@@ -1,5 +1,27 @@
 jQuery(document).ready(function($){
 
+	$(window).ready(function(){
+	  setInterval(function(){ 
+	    $('body').addClass("load")
+	  }, 2000);
+	
+	});
+	
+	$('#newsletter-close').on('click', function(){
+	    $('#newsletter-bg').removeClass('open');
+	    $('#newsletter').removeClass('open');
+	});
 
+	$('#signup-submit').on('click', function(){
+	    $('#newsletter').removeClass('open');
+	    $('#thankyou').addClass('show');
+	});	
+	$('#thankyou-close').on('click', function(){
+	    $('#thankyou').removeClass('show');
+	    $('#newsletter-bg').removeClass('open');
+	});
+	
+
+	
 
 });
