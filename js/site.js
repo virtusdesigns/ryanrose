f60f6a38143763d476adf3d5ab8e0e0d8dff47bb
Brett Bodine <brett@virtusdesigns.com>
2017-08-01 16:16:11 -0700

    update transitions

--- js/site.js
+++ js/site.js
@@ -12,12 +12,23 @@ jQuery(document).ready(function($){
 	    $('#newsletter').removeClass('open');
 	});
 
-	$('#thankyou-close').on('click', function(){
-	    $('#thankyou').removeClass('show');
+	$('#thankyou-close').on('click', function(){;
 	    $('#newsletter-bg').removeClass('open');
+	    $('#thankyou').addClass('fadeout');
 	});
 	
-
+	$('#form').one('submit',function(){
+	        var inputq1 = encodeURIComponent($('#signup-email').val());
+	        var q1ID = "entry.2808744";
+	
+	        var baseURL = 'https://docs.google.com/a/ryanrosewine.com/forms/d/e/1FAIpQLScDWEZrcYpw18Kbpy2GFPb5-m1uODiKtln_foJ3x2nyllPTFA/formResponse?';
+	        var submitRef = '&submit=Submit';
+	        var submitURL = (baseURL + q1ID + "=" + inputq1 + submitRef);
+	        console.log(submitURL);
+	        $(this)[0].action=submitURL;
+	        $('#newsletter').addClass('fadeout');
+	        $('#thankyou').addClass('show');
+	    });
 	
 
 	
